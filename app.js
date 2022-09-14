57d8d31d4febb9cb2fda5602ae05e8db586409aa
Zay Min Maw <59911091+zay467@users.noreply.github.com>
2022-09-14 09:41:00 +0630

    feat: initialized express app and sync db

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const db = require("./models");
+const { PORT } = require("./config/env.config");
+
+const app = express();
+
+db.sequelize
+  .sync()
+  .then(() => {
+    console.log("Synced db.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Service is running on port ${PORT}`);
+});
