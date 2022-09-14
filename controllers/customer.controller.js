6983eaee20bd4e8dc3136c0891dd0e3401e8a22f
Zay Min Maw <59911091+zay467@users.noreply.github.com>
2022-09-14 11:12:25 +0630

    feat: implemented controller function for routes

--- controllers/customer.controller.js
+++ controllers/customer.controller.js
@@ -2,12 +2,73 @@ const db = require("../models");
 const Customer = db.customer;
 const Op = db.Sequelize.Op;
 
-exports.create = (req, res) => {};
+exports.create = (req, res) => {
+  Customer.create(req.body)
+    .then((data) => res.json(data))
+    .catch((err) =>
+      res.status(500).json({ message: err.message || "Something went wrong." })
+    );
+};
 
-exports.readAll = (req, res) => {};
+exports.readAll = (req, res) => {
+  Customer.findAll()
+    .then((data) => res.json(data))
+    .catch((err) =>
+      res.status(500).json({ message: err.message || "Something went wrong." })
+    );
+};
 
-exports.readOne = (req, res) => {};
+exports.readOne = (req, res) => {
+  const id = req.params.id;
+  Customer.findByPk(id)
+    .then((data) => {
+      if (data) {
+        res.json(data);
+      } else {
+        res.status(404).json({ message: "Not Found." });
+      }
+    })
+    .catch((err) =>
+      res.status(500).json({ message: err.message || "Something went wrong." })
+    );
+};
 
-exports.update = (req, res) => {};
+exports.update = (req, res) => {
+  const id = req.params.id;
+  Customer.update(req.body, { where: { id: id } })
+    .then((num) => {
+      if (num == 1) {
+        res.json({
+          message: "Customer update successful.",
+        });
+      } else {
+        res.json({
+          message: "Customer update failed.",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message || "Something went wrong." });
+    });
+};
 
-exports.delete = (req, res) => {};
+exports.delete = (req, res) => {
+  const id = req.params.id;
+  Customer.destroy({
+    where: { id: id },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.json({
+          message: "Customer delete successful.",
+        });
+      } else {
+        res.json({
+          message: "Customer delete failed.",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message || "Something went wrong." });
+    });
+};
